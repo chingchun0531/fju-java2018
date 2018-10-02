0504a6aeff632b3b15bd56481cf7eac20d428688
user <user@es302n19>
2018-10-02 10:49:42 +0800

    variables

--- my project/src/com/tom/Hello.java
+++ my project/src/com/tom/Hello.java
@@ -4,6 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+    int age = 18;
+    float weight = 50f;
+    float height = 153f;
+    System.out.println(age);
+    System.out.println(weight);
+    System.out.println(height);
 
 	}
 
