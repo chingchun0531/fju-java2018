8031c404115f65ce222599394357045824f0aed6
user <user@es302n19>
2018-10-02 11:03:46 +0800

    exam2

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -4,7 +4,19 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+    int number = 9;
+    int age = 18;
+    float weight = 52.5f;
+    float height = 153.5f;
+    double rightsight = 1;
+    double leftsight = 0.4;
+    System.out.println(number);
+    System.out.println(age);
+    System.out.println(weight);
+    System.out.println(height);
+    System.out.println(rightsight);
+    System.out.println(leftsight);
+    
 	}
 
 }
