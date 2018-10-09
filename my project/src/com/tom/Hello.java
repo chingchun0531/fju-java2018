94c658d09a6089c7dfad5f2b8b48cb1d6fae1a64
user <user@es302n13>
2018-10-09 11:58:19 +0800

    person

--- my project/src/com/tom/Hello.java
+++ my project/src/com/tom/Hello.java
@@ -4,12 +4,24 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-    int age = 18;
-    float weight = 50f;
-    float height = 153f;
-    System.out.println(age);
-    System.out.println(weight);
-    System.out.println(height);
+
+		Person p = new Person(50f, 1.53f);
+		// p.weight = 50f;
+		// p.height = 1.53f;
+		System.out.println(p.bmi());
+		p.hello();
+		Person hank = new Person(65.5f, 1.7f);
+		// hank.weight = 65.5f;
+		// hank.height = 1.7f;
+		System.out.println(hank.bmi());
+		// Person jack = null;
+		// jack.hello();
+
+		/*
+		 * int age = 18; float weight = 50f; float height = 1.53f;
+		 * System.out.println(age); System.out.println(weight);
+		 * System.out.println(height);
+		 */
 
 	}
 
